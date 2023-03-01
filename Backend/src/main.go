e72ef17be791ce43209435342ed4a381490d9e7f
Lgernier <93293855+Lgernier@users.noreply.github.com>
2023-03-01 18:10:02 -0500

    removed line with depricated function

--- Backend/src/main.go
+++ Backend/src/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	initOracle()
+	testGorm()
 
-	fmt.Print("fail")
 	host := "127.0.0.1:8080"
 	if err := http.ListenAndServe(host, httpHandler()); err != nil {
 		log.Fatalf("Failed to listen on %s: %v", host, err)
