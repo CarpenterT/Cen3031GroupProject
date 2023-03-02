304e7553b438be8e78396914dba73a629cd58344
CarpenterT <carpentert@ufl.edu>
2023-03-01 21:38:11 -0500

    Added 2 Cypress tests.

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,24 +1,34 @@
 import { Component } from '@angular/core';
+import { MatCard } from '@angular/material/card';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
+  
 })
 export class LoginPageComponent {
+  
   username: string = '';
   password: string = '';
+
   constructor(private router: Router){
 
   }
+
+  
   submit() {
+    //The alert menu does not update properly in Cypress. Works in normal operation though.
     window.alert('Username is: "' + this.username + '". Password is: "' + this.password + '".')
-    this.clear()
+    //The below line causes a huge error in Cypress.
     this.router.navigate(['/login'], { queryParams: { username: this.username, password: this.password } });
+
+    this.clear()
+    
   }
   clear() {
-    //this.username = '';
-    //this.password = '';
+    this.username = '';
+    this.password = '';
   }
 }
