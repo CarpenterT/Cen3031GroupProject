eb995fa7547a2a85ea5c0be6abc4da36c79f972e
CarpenterT <carpentert@ufl.edu>
2023-03-01 19:44:48 -0500

    changed URL to update with login info

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -8,13 +9,16 @@ import { Component } from '@angular/core';
 export class LoginPageComponent {
   username: string = '';
   password: string = '';
+  constructor(private router: Router){
 
+  }
   submit() {
     window.alert('Username is: "' + this.username + '". Password is: "' + this.password + '".')
     this.clear()
+    this.router.navigate(['/login'], { queryParams: { username: this.username, password: this.password } });
   }
   clear() {
-    this.username = '';
-    this.password = '';
+    //this.username = '';
+    //this.password = '';
   }
 }
