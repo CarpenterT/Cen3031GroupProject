8510e76a7a399e56b91e06df0bc3fd4f16d8a0e6
drosabal <dmrosabal@gmail.com>
2023-04-19 19:17:07 -0400

    Added request to create server, display server list

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -4,6 +4,7 @@ import { UserService } from '../services/user.service';
 import { Server } from '../models/server.model';
 import { User } from '../models/user.model';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home=page',
@@ -11,19 +12,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  user: User = {} as User;
+  //user: User = {} as User;
   username: string = '';
-  servers: Server[] = [];
+  //servers: Server[] = [];
+  serverIDs: number[] = [];
   newServerName: string = '';
+  serverName: Record<number, string> = {};
 
-  constructor(private router: Router, private serverService: ServerService, private userService: UserService) { }
+  constructor(private router: Router, private http: HttpClient, private serverService: ServerService, private userService: UserService) { }
 
   ngOnInit(): void {
     if (!localStorage.getItem('currentUser') || localStorage.getItem('currentUser') == '') {
       this.router.navigate(['/']);
+    } else {
+      this.username = localStorage.getItem('currentUser') || '';
+      this.http.get('http://localhost:8080/users/user/' + this.username + '/servers').subscribe((serversString: any) => {
+        this.serverIDs = serversString.split(',').map(Number);
+      })
+      for (var serverID in this.serverIDs) {
+        this.http.get('http://localhost:8080/servers/server/' + serverID).subscribe((serverName: any) => {
+          this.serverName[serverID] = serverName;
+        })
+      }
     }
-    this.username = localStorage.getItem('currentUser') || '';
-    // TODO: fix so that user is retrieved as an object. Need list of servers that user is a member of.
+    // TODO: change so that user is retrieved as an object. Need list of servers that user is a member of.
     /*
     this.userService.getUser(localStorage.getItem('currentUser') || '').subscribe((user: User) => {
       this.user = user;
@@ -34,8 +46,9 @@ export class HomePageComponent implements OnInit {
     */
   }
 
-  onCreateServer() {
-    this.serverService.createServer(this.newServerName, this.user.username);
+  onCreateServer(name: string, admin: string) {
+    //this.serverService.createServer(this.newServerName, this.username);
+    this.http.post('http://localhost:8080/servers', { name, admin });
     this.newServerName = '';
   }
 
