86f28bc58b1f5deb3094bb0616d023446550ba5c
Lgernier <93293855+Lgernier@users.noreply.github.com>
2023-04-19 22:48:03 -0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+func initializeRouter() {
+	r := mux.NewRouter()
+
+	//defining the functions needed to handle users
+	r.HandleFunc("/users", GetUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", GetUserByID).Methods("GET")
+	r.HandleFunc("/users/user/{username}", GetUserByName).Methods("GET")
+	r.HandleFunc("/users", CreateUser).Methods("POST")
+	r.HandleFunc("/users/user", CheckPass).Methods("POST")
+	r.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+
+	//defining the functions needed to handle servers
+	r.HandleFunc("/servers", GetServer).Methods("GET")
+	r.HandleFunc("/servers/{id}", GetServerByID).Methods("GET")
+	r.HandleFunc("/servers/servers/{servername}", GetServerByName).Methods("GET")
+	r.HandleFunc("/servers", CreateServer).Methods("POST")
+	//r.HandleFunc("/server/user", CheckPass).Methods("POST")
+	r.HandleFunc("/servers/{id}", UpdateServer).Methods("PUT")
+	r.HandleFunc("/servers/{id}", DeleteServer).Methods("DELETE")
+
+	//this line fixes issues with cross origin garbage. Finally.
+	handler := cors.AllowAll().Handler(r)
+
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
+
+// To run, cd into the backend/src folder, type "go build", then ./m.exe or whatever the executable is named for you.
+// This gets the server running, and in the web app you can now add to the database.
+// We are writing to ClusterC.db in backend/src/database. Do not hard-code your full path. See user.go.
+func main() {
+	InitialMigration()
+	InitialServerMigration()
+	initializeRouter()
+
+	//unit tests go here
+}
